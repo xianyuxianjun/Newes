f559fa79cdf644a1399d97f3ebd062cdf3bc2442
咸鱼仙君 <1796792003@qq.com>
2024-06-14 17:02:44 +0800

    测试登录，注册无问题

--- src/main/java/com/chenxianyu/newes/Text/Text.java
+++ src/main/java/com/chenxianyu/newes/Text/Text.java
@@ -1,10 +1,17 @@
 package com.chenxianyu.newes.Text;
+import com.chenxianyu.newes.Pojo.User;
 import com.chenxianyu.newes.Service.*;
 
 public class Text {
     public static void main(String[] args) {
         UserService userService = new UserService();
         //测试login方法
-        System.out.println(userService.login("user1", "password1"));
+        System.out.println(userService.login("user", "password1"));
+
+        //测试register方法
+        User user = new User();
+        user.setUsername("newuser");
+        user.setPassword("newpassword");
+        System.out.println(userService.register(user));
     }
 }
---
+++ src/test/java/com/chenxianyu/newes/Text/UserServiceTest.java
@@ -0,0 +1,51 @@
+package com.chenxianyu.newes.Text;
+
+import com.chenxianyu.newes.Pojo.User;
+import com.chenxianyu.newes.Service.UserService;
+import org.junit.jupiter.api.BeforeEach;
+import org.testng.annotations.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class UserServiceTest {
+    private UserService userService;
+
+    @BeforeEach
+    void setUp() {
+        userService = new UserService();
+    }
+
+    @Test
+    void login() {
+        assertTrue(userService.login("user1", "password1"));
+        assertFalse(userService.login("user", "wrongpassword"));
+    }
+
+    @Test
+    void register() {
+        User user = new User();
+        user.setUsername("newuser");
+        user.setPassword("newpassword");
+        assertTrue(userService.register(user));
+    }
+
+    @Test
+    void updatePassword() {
+        assertTrue(userService.updatePassword(1, "newpassword"));
+    }
+
+    @Test
+    void updateUser() {
+        User user = new User();
+        user.setId(1);
+        user.setUsername("updateduser");
+        assertTrue(userService.updateUser(user));
+    }
+
+    @Test
+    void getUser() {
+        User user = userService.getUser(1);
+        assertNotNull(user);
+        assertEquals(1, user.getId());
+    }
+}
